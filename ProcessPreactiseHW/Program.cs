6dc230e72c7a68c93a64c44994d90765aab40888
Jonatan Lifshits <djonli2003@gmail.com>
2023-08-07 21:15:49 +0300

    Initial commit process

---
+++ ProcessPreactiseHW/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Diagnostics;
+
+Run();
+
+void Run() 
+{ 
+    while (true) 
+    {
+        Console.WriteLine("1.Get current process\n2.\n");
+
+        string? input = null;
+
+        input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                GetCurrentProcessName();
+                break;
+            case "2":
+
+                break;
+            case "3":
+
+                break;
+            case "4":
+
+                break;
+        }
+    }
+}
+
+
+void GetCurrentProcessName() 
+{
+    try 
+    {
+        Process p = Process.GetCurrentProcess();
+        Console.WriteLine($"\nOutput:\n{p.ProcessName}\n");
+    }
+    catch(Exception ex) 
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+
+
