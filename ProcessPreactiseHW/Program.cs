4232dfd1737c3f4ce5fb81f87819e70c1bf8506f
Jonatan Lifshits <djonli2003@gmail.com>
2023-08-07 22:16:02 +0300

    mod: Program.cs

--- ProcessPreactiseHW/Program.cs
+++ ProcessPreactiseHW/Program.cs
@@ -7,7 +7,7 @@ void Run()
 { 
     while (true) 
     {
-        Console.WriteLine("1.Get current process\n2.\n");
+        Console.WriteLine("1.Get current process\n2.Find Process by name\n3.Start process\n");
 
         string? input = null;
 
@@ -19,13 +19,10 @@ void Run()
                 GetCurrentProcessName();
                 break;
             case "2":
-
+                FindProcessByName();
                 break;
             case "3":
-
-                break;
-            case "4":
-
+                StartProcessByName();
                 break;
         }
     }
@@ -36,7 +33,7 @@ void GetCurrentProcessName()
 {
     try 
     {
-        Process p = Process.GetCurrentProcess();
+        using Process p = Process.GetCurrentProcess();
         Console.WriteLine($"\nOutput:\n{p.ProcessName}\n");
     }
     catch(Exception ex) 
@@ -45,5 +42,40 @@ void GetCurrentProcessName()
     }
 }
 
+void FindProcessByName() 
+{
+    try { 
+        string? processName = null;
+    
+        Console.WriteLine("Enter the process name: ");
+        processName = Console.ReadLine();
 
+        Process[] p = Process.GetProcessesByName(processName);
 
+        foreach (Process proc in p)
+        {
+            Console.WriteLine($"{proc.Id} {proc.ProcessName} {proc.MachineName} {proc.BasePriority}\n");
+        }
+    }
+    catch(Exception ex) 
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+void StartProcessByName() 
+{
+    try
+    {
+        string? processName = null;
+
+        Console.WriteLine("Enter the process name:\n");     
+        processName = Console.ReadLine();
+
+        using Process p = Process.Start(processName);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
